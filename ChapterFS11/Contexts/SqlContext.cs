2200388a1f7916f358616dca0c4540bc94c6caa9
r3n470 <rluiz1808@gmail.com>
2023-05-21 02:00:27 -0300

    metodo get api

---
+++ ChapterFS11/Contexts/SqlContext.cs
@@ -0,0 +1,26 @@
+﻿using ChapterFS11.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChapterFS11.Contexts
+{
+    public class Sqlcontext : DbContext
+    {
+        public Sqlcontext() { }
+        public Sqlcontext(DbContextOptions<Sqlcontext> options) : base(options) { }
+        // vamos utilizar esse método para configurar o banco de dados
+        protected override void
+           OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                // cada provedor tem sua sintaxe para especificação
+                //optionsBuilder.UseSqlServer("Data Source = DESKTOP-6MI3UBS\\SQLEXPRESS;; initial catalog = Chapter; user id = sa; password = 123");
+                optionsBuilder.UseSqlServer("Data Source = DESKTOP-6MI3UBS\\SQLEXPRESS;; initial catalog = Chapter; Integrated Security = true; TrustServerCertificate=True");
+                //optionsBuilder.UseSqlServer("Data Source = DESKTOP-6MI3UBS\\SQLEXPRESS; initial catalog = Chapter; Integrated Security = true");
+            }
+        }
+        // dbset representa as entidades que serão utilizadas nas operações de leitura, criação, atualização e deleção
+        public DbSet<Livro> Livros { get; set; }
+
+    }
+}
---
+++ ChapterFS11/Repositories/LivroRepository.cs
@@ -0,0 +1,22 @@
+﻿using ChapterFS11.Contexts;
+using ChapterFS11.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChapterFS11.Repositories
+{
+    public class LivroRepository
+    {
+        private readonly Sqlcontext _context;
+        public LivroRepository(Sqlcontext context)
+
+        {
+            _context = context;    
+        }
+
+        public List<Livro> Listar()
+        {
+            return _context.Livros.ToList();
+        }
+
+    }
+}
